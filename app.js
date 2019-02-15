094a4ec0b0b833910d0581d96338d83da91a215b
Lefebvre Jules <juleslefebvre.pro@outlook.fr>
2019-02-15 17:08:03 +0100

    clean all file

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-/**
-* @file app.js
-* @author Jules Lefebvre <juleslefebvre.10@outlook.fr>
-* @date 2019/01/06
-* @project ninotaurus (https://github.com/JulesdeCube/ninotaurus).
-* @copyright Copyright (c) 2019 Jules Lefebvre.
-* 
-* @license GNU-GPL
-*  This file is part of ninotaurus (https://github.com/JulesdeCube/ninotaurus).
-*  Copyright (c) 2019 Jules Lefebvre.
-*  ninotaurus is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-*  
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*  
-*  You should have received a copy of the GNU General Public License
-*  along with ninotaurus.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 //module
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-var fs = require("fs");
+const fs = require("fs");
 
 //init module
 const application = express();
@@ -35,98 +11,21 @@ const sockets = socketIo(server);
 
 //parameter
 const serverPort = 8080;
-const configFilePath = './server/map/defautMap.json';
-var configFile;
+
+
 //express config
 application.use('/', express.static(__dirname + '/client'));
 
 //soket.io config
-fs.readFile(configFilePath,(error, configFile) => {
-  if(!error){
-    try {
-      configFile = JSON.parse(configFile);
-      console.log(configFile);
-      
-      refreshSpawn();
-    } catch (error) {
-      serverError('invalide config File: ' + error)
-    }
+sockets.on('connection', function(socket){
+  console.log('connected');
 
-    sockets.on('connection', function(socket){
-      console.log(clientLogHeader(socket) + 'connected');
-      
-      socket.on('game', function(request){
-        if (request.type == 'get') {
-          switch (request.message) {
-            case 'config':
-              socket.emit('game', {
-                type: 'post',
-                request: request,
-                message:configFile
-              });
-            break;
-            
-            default:
-              clientError('Error: invalide message', socket, request);
-            break;
-          }
-        } else {
-          clientError('Error: invalide message\'s type', socket, request);
-        }  
-      });
-      
-      socket.on('disconnect', function(){
-        console.log(clientLogHeader(socket) + 'user disconnected');
-      });
-    });
-    
-    //run server
-    server.listen(serverPort, () => {
-      console.log('Server started on port: ' + serverPort);
-    });
-  } else {
-    serverError('config file can\'t be charge\n' + error);
-  }
+  socket.on('disconnect', function(){
+    console.log('disconnect');
+  });
 });
 
-function serverLogHeader() {
-  return  'Server: '; 
-}
-
-function serverError(message){
-  console.error(serverLogHeader() + message + '\n');
-}
-
-function clientLogHeader(inputSocket) {
-  return inputSocket.id + ' - ' + inputSocket.conn.remoteAddress + ': '; 
-}
-
-function clientError(message, senderSocket, sendedRequest){
-  senderSocket.emit('game',
-  {
-    type: 'Error',
-    message: message
-  }
-  );
-  console.error(clientLogHeader(senderSocket) + message , sendedRequest, '\n');
-}
-
-function refreshSpawn(){
-  configFile.players.forEach(player => {
-    if (!player.hasOwnProperty('caracters')) {
-      player.caracters = [];
-      player.spawns.forEach(spawn => {
-
-        console.log(configFile);
-        if (player.caracters.length < configFile.caracterPerPlayer) {
-          player.caracters.push({
-            x: spawn.x,
-            y: spawn.y,
-          });
-        }
-      });
-    } else {
-
-    }
-  });
-}
+//run server
+server.listen(serverPort, () => {
+  console.log('Server started on port: ' + serverPort);
+});
--- client/js/game.js
+++ client/js/game.js
@@ -1,141 +1,14 @@
 var socket = io();
 
-var config;
+//socket.on('game', function(msg){});
 
-
-var caseWidth = 25;
+//socket.emit('game',{});
 
 function setup() {
-
-  socket.on('game', function(msg){
-    switch (msg.request.message) {
-      case 'config':
-        config = msg.message;
-        autoResize(config.map);
-      break;
-
-      default:
-        console.log(msg);
-      break;
-    }
-  });
-
-  socket.emit('game',{
-    type:'get',
-    message:'config'
-  });
-  
   createCanvas(0, 0);
-  autoResize(config.map);
 }
 
 
 function draw() {
-if (config) {
-  
-  clear();
-  drawMap(config.map);
-  drawSpawns(config.players);
-  drawArrives(config.players);
-  drawCaracters(config.players);
-  drawCursor();
-}
-}
 
-
-function autoResize(map) {
-  let mapDimention = maxDimentionMap(map);
-  resizeCanvas(mapDimention.x * caseWidth, mapDimention.y * caseWidth);
-}
-
-function maxDimentionMap (map) {
-  let maxY = 0;
-  for (let l = 0; l < map.length; l++) {
-    if (map[l].length > maxY) {
-      maxY = map[l].length;
-    }
-  }
-  return {
-    x: map.length,
-    y: maxY
-  };
-}
-
-function drawMap (map) {
-  for (let l = 0; l < map.length; l++) {
-    for (let c = 0; c < map[l].length; c++) {
-      noStroke();
-      switch (map[l][c]) {
-        case '█':
-        fill('#27ae60');
-        break;
-        case '▓':
-        fill('#2ecc71');
-        break;
-        case '▒':
-        fill('#34495e');
-        break;
-        case 'M':
-        fill('#162029');
-        break;
-        default:
-        stroke(51, 20);
-        strokeWeight(1);
-        fill(0, 0);
-        break;
-      }
-      rect(c * caseWidth, l * caseWidth, caseWidth, caseWidth);
-    }
-  }
-}
-
-function cursorPosition() {
-  return {
-    x: mouseX -(mouseX % caseWidth),
-    y: mouseY -(mouseY % caseWidth)
-  };
-}
-
-function drawCursor () {
-  noStroke();
-  fill(0, 25);
-  rect(cursorPosition().x, cursorPosition().y, caseWidth, caseWidth);
-}
-
-function drawCaracters (playerList) {
-  console.log(config);
-  
-  playerList.forEach(player => {
-    player.caracters.forEach(caracter => {
-      drawCase(caracter.x, caracter.y, "#000");
-    });
-  });
-}
-
-function drawSpawns (playerList) {
-  playerList.forEach(player => {
-    player.spawns.forEach(spawn => {
-      drawCase(spawn.x, spawn.y, player.color);
-    });
-  });
-}
-
-function drawArrives (playerList) {
-  playerList.forEach(player => {
-    player.arrives.forEach(arrive => {
-      drawCase(arrive.x, arrive.y, player.color);
-    });
-  });
-}
-
-function drawCase (x, y, color) {
-  noStroke();
-  fill(color);
-  rect(caseWidth * x , caseWidth * y, caseWidth, caseWidth);
-}
-
-function drawCaracter (x, y, color) {
-  noStroke();
-  fill(color);
-  circle(caseWidth * x + caseWidth / 2, caseWidth * y + caseWidth / 2, caseWidth / 2);
 }
