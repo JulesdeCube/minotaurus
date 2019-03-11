3938a907e2c5951de4fe859c6af4850f53c31be5
Lefebvre Jules <juleslefebvre.pro@outlook.fr>
2019-03-11 22:23:57 +0100

    clean all

--- app.js
+++ app.js
@@ -2,54 +2,20 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const minotaurus = require("./minotaurus/minotaurus");
-const database = require("./database/database");
+
+//parameter
+const SERVERPORT = 8080;
 
 //init module
 const application = express();
 const server = http.Server(application);
 const sockets = socketIo(server);
-const minotaurusServ = minotaurus.Server(sockets);
-//parameter
-const serverPort = 8080;
 
-
-userConnectionSockets = sockets.of('/sign');
-
-userConnectionSockets.on('connection', (socket) => {
-  console.log('sign conection');
-  
-  socket.on('signup', (information) => {
-    console.log(information);
-    if (typeof information === 'object') {
-      if(
-        information.hasOwnProperty('username') && typeof information.username === 'string' && information.username !== '' &&
-        information.hasOwnProperty('email') && typeof information.username === 'string' && information.username !== '' &&
-        information.hasOwnProperty('password') && typeof information.username === 'string' && information.username !== '' &&
-        information.hasOwnProperty('passwordRepeat') && typeof information.username === 'string' && information.username !== ''
-      ){
-        if (information.password === information.passwordRepeat) {
-          console.log('goot');
-          
-        }
-      }
-    }
-  });
-});
 
 //express config
 application.use('/', express.static(__dirname + '/client'));
 
-/* //soket.io config
-sockets.on('connection', function(socket){
-  console.log('connected');
-
-  socket.on('disconnect', function(){
-    console.log('disconnect');
-  });
-});
- */
 //run server
-server.listen(serverPort, () => {
-  console.log('Server started on port: ' + serverPort);
+server.listen(SERVERPORT, () => {
+  console.log('Server started on port: ' + SERVERPORT);
 });
