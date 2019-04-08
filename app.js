ec88a49ef91a84b2932a79ee3062e9116d62b24b
Lefebvre Jules <juleslefebvre.pro@outlook.fr>
2019-04-08 08:31:24 +0200

    basic of singup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const crypto = require('crypto');
+const dataBase = require('./lib/dataBase');
+
 
 //parameter
 const SERVERPORT = 8080;
@@ -10,12 +13,66 @@ const SERVERPORT = 8080;
 const application = express();
 const server = http.Server(application);
 const sockets = socketIo(server);
+const userdatabase = new dataBase('./database/user.json');
+
 
 sockets.of('/sign').on('connection',(socket) => {
   socket.on('post',(msg) => {
-    console.log(msg);
+
+    if (msg.header === 'signUp') {
+      if (Object.keys(msg.content).length === 4) {
+        let key = ['username', 'email', 'password', 'passwordRepeat'];
+        let isGood = true;
+        for (let i = 0; i < 4 && isGood; i++) {
+          if (!msg.content.hasOwnProperty(key[i])) {
+            isGood = false;
+          }
+          if (typeof msg.content[key[i]] !== 'string' || msg.content[key[i]] === '') {
+            isGood = false;
+          }
+        }
+        
+        if (!validateEmail(msg.content.email)) {;  
+          isGood = false;
+        }
+        if (msg.content.password !== msg.content.passwordRepeat) {
+          isGood = false;
+        }
+        if (userdatabase.search('email', msg.content.email) !== -1) {
+          
+          isGood = false;
+          socket.emit({
+            type: 'error',
+            content: 'emailDouble'
+          })
+        }
+        if (userdatabase.search('userName', msg.content.username) !== -1) {
+          isGood = false;
+          socket.emit({
+            type: 'error',
+            content: 'usernameDouble'
+          })
+        }
+        if (isGood) {
+          userdatabase.add(
+            {
+              userName: msg.content.username,
+              email: msg.content.email,
+              password: crypto.createHmac('sha256', msg.content.password).update('petitgrindesable').digest('hex')
+            }
+          )
+        }
+      }
+    }
   })
 });
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+
 //express config
 application.use('/', express.static(__dirname + '/client'));
 
--- client/minotaurus/index.js
+++ client/minotaurus/index.js
@@ -1,24 +1,27 @@
 window.onload = () => {
   let socket = io('/sign');
-
+  
   var tabs = {
     signIn: document.getElementById('signIn'),
     signUp: document.getElementById('signUp')
   }
-
   
-
-
-
+  
+  
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
   function Tabshidden(){
     for (const tabsName in tabs) {
       tabs[tabsName].hidden = true;
     }
   }
-
+  
   function addEventToClass(className, event, callBack){
     balises = document.getElementsByClassName(className);
-
+    
     for (const balise in balises) {
       if (typeof balises[balise] === 'object') {
         balises[balise].addEventListener(event, callBack); 
@@ -26,11 +29,11 @@ window.onload = () => {
       
     }
   }
-
-  function socketFormSend(formId, socket){
+  
+  function socketFormSend(formId, socket, isCorrect){
     let form = document.getElementById(formId);
     form.onsubmit = (event) => {
-    
+      
       event.preventDefault();
       let inputs = event.originalTarget.querySelectorAll('input');
       
@@ -39,22 +42,24 @@ window.onload = () => {
         inputValue[input.name] = input.value;
       });
       
-      socket.emit('post',{
-        header:event.originalTarget.name,
-        content:inputValue
-      });
+      if(isCorrect(inputValue)){
+        socket.emit('post',{
+          header:event.originalTarget.name,
+          content:inputValue
+        });
+      }
     };  
   }
-
-
-
-
+  
+  
+  
+  
   Tabshidden();
   tabs.signIn.hidden = false;
   
-
-
-
+  
+  
+  
   addEventToClass('display-SignUp','click',() => {
     Tabshidden();
     tabs.signUp.hidden = false;
@@ -63,11 +68,50 @@ window.onload = () => {
     Tabshidden();
     tabs.signIn.hidden = false;
   })
-
-
-
-  socketFormSend('signIn-form', socket);
-  socketFormSend('signUp-form', socket);
-
+  
+  
+  document.getElementById('signUp-passwordRepeat').oninput = (Event) => {
+    document.getElementById('signUp-passwordRepeat').classList.remove('invalid');
+    document.getElementById('signUp-password').classList.remove('invalid');
+  }
+  document.getElementById('signUp-password').oninput = (Event) => {
+    document.getElementById('signUp-passwordRepeat').classList.remove('invalid');
+    document.getElementById('signUp-password').classList.remove('invalid');
+  }
+  document.getElementById('signUp-email').oninput = (Event) => {
+    document.getElementById('signUp-email').classList.remove('invalid');
+  }
+  
+  
+  socketFormSend('signIn-form', socket, (input) => {
+    console.log(input);
+    
+  });
+  socketFormSend('signUp-form', socket, (input) => {
+    let isGood = true;
+    for (const key in input) {
+      document.getElementById('signUp-' + key + '-Message').innerHTML = '';
+        document.getElementById('signUp-' + key).classList.remove('invalid');
+      if (input[key] === '' && key !== 'passwordRepeat') {
+        document.getElementById('signUp-' + key + '-Message').innerHTML = 'you need to enter a ' + key + '.';
+        document.getElementById('signUp-' + key).classList.add('invalid');
+        isGood = false;
+      }
+    }
+    if (!validateEmail(input.email)) {
+      document.getElementById('signUp-email').classList.add('invalid');
+      isGood = false;
+    }
+    if (input.password !== input.passwordRepeat) {
+      document.getElementById('signUp-passwordRepeat-Message').innerHTML = 'passworld are not the same';
+      document.getElementById('signUp-passwordRepeat').classList.add('invalid');
+      document.getElementById('signUp-passwordRepeat').value = '';
+      isGood = false;
+    }
+    console.log(input);
+    
+    return isGood;
+  } );
+  
   
 };
